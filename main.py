172fa71672967fcb98e75aeb1d31b4e0c587a428
Abe280 <72824753+Abe280@users.noreply.github.com>
2021-01-14 13:18:47 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+if input.light_level() > 5:
+    music.siren.play()
+else:
+    music.magic_wand.loop()
---
+++ main.ts
@@ -0,0 +1,6 @@
+if (input.lightLevel() > 5) {
+    music.siren.play()
+} else {
+    music.magicWand.loop()
+}
+
