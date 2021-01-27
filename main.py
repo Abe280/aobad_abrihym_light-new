fdc4605510b68fe9fa8544554a2a3023593d22e5
Abe280 <72824753+Abe280@users.noreply.github.com>
2021-01-27 09:56:27 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,9 +3,5 @@ while True:
         light.set_all(light.rgb(0,0,255))
     if input.temperature(TemperatureUnit.FAHRENHEIT) <82:
         light.set_all(light.rgb(0,0,255))
-    if input.temperature(TemperatureUnit.FAHRENHEIT) <78:
-        light.set_all(light.rgb(0,255,0))
-    if input.temperature(TemperatureUnit.FAHRENHEIT) >82:
-        light.set_all(light.rgb(0,255,0))
-    if input.sound_level() >110:
+    if input.sound_level() > 120:
         light.set_all(light.rgb(255, 0, 0))
--- main.ts
+++ main.ts
@@ -7,15 +7,7 @@ while (true) {
         light.setAll(light.rgb(0, 0, 255))
     }
     
-    if (input.temperature(TemperatureUnit.Fahrenheit) < 78) {
-        light.setAll(light.rgb(0, 255, 0))
-    }
-    
-    if (input.temperature(TemperatureUnit.Fahrenheit) > 82) {
-        light.setAll(light.rgb(0, 255, 0))
-    }
-    
-    if (input.soundLevel() > 110) {
+    if (input.soundLevel() > 120) {
         light.setAll(light.rgb(255, 0, 0))
     }
     
